87aa0d712617b9367ad8116dea36c21a3063eb5f
pleomax13 <43319882+pleomax13@users.noreply.github.com>
2019-03-23 16:33:55 +0300

    Add files via upload

---
+++ precache-manifest.c8244fc0a66c33a67a215512b360b56f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9b4c46be255ecd26bce",
+    "url": "/todo/static/css/main.e64dfe1e.chunk.css"
+  },
+  {
+    "revision": "b9b4c46be255ecd26bce",
+    "url": "/todo/static/js/main.b9b4c46b.chunk.js"
+  },
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "25a318c798e8fc7f4e8b",
+    "url": "/todo/static/js/2.25a318c7.chunk.js"
+  },
+  {
+    "revision": "8e2579c031f58e3a5aea8d5a0fd994db",
+    "url": "/todo/index.html"
+  }
+];
